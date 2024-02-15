a2c08a44eb3b8431b70f0f98fba1a5750c53102b
Marigela Bhavana <marigelabhavana2000@gmail.com>
2024-02-15 12:08:32 +0530

    Success

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Feb15WelcomeApp1;
+
+Console.WriteLine("Welcome to Authentication App");
+SignIn obj=new SignIn();
+Console.WriteLine(obj.Authenticate("admin", "admin@12345"));
+
---
+++ SignIn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feb15WelcomeApp1
+{
+     public class SignIn
+    {
+        public string Authenticate(string username, string password)
+        {
+            string msg;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                msg = "Username and Password required";
+            }
+            else
+            {
+                if ((username == "admin") && (password == "admin@12345"))
+                {
+                    msg = "Authentication Pass";
+                }
+                else
+                {
+                    msg = "Authentication Fail";
+                }
+
+            }
+            return msg;
+        }
+    }
+}
